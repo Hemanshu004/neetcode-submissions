0ca64469f80ef73400ada95b75c8c9faf916ea06
HemanshuPatel <159682883+Hemanshu004@users.noreply.github.com>
2026-07-31 12:01:46 +0530

    Add: word-break-ii - submission-1

---
+++ Data Structures & Algorithms/word-break-ii/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        cache={}
+        word_s=set(wordDict)
+        def dfs(i):
+            if i==len(s):
+                return [""]
+            if i in cache:
+                return cache[i]
+            
+            res=[]
+            for j in range(i,len(s)):
+                w=s[i:j+1]
+                if w not in word_s:
+                    continue
+                
+                strings=dfs(j+1)
+                for substr in strings:
+                    sentence=w
+                    if substr:
+                        sentence+=" "+substr
+                    res.append(sentence)
+            cache[i]=res
+            return res
+        return dfs(0)
+                   
+        
