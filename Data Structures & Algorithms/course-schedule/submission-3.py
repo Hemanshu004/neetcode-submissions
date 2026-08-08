e7a6c4e4f8b9abf6c4d77ee719880a93dc84c7fd
HemanshuPatel <159682883+Hemanshu004@users.noreply.github.com>
2026-08-08 10:47:37 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        # [0]=[1]
+        courses={i:[] for i in range(numCourses)}
+        for x,y in prerequisites:
+            courses[x].append(y)
+
+
+        visit=set()
+        def dfs(crs):
+            if crs in visit:
+                return False
+            
+            if courses[crs]==[]:
+                return True
+            visit.add(crs)
+
+            for drs in courses[crs]:
+                if not dfs(drs):
+                    return False
+            visit.remove(crs)
+            courses[crs]=[]
+            return True
+
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
